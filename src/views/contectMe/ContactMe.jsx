79edf38db94a9a68c4aa418308fc0e7d4e86c5ef
yardencohavi <cohaviyarden@gmail.com>
2021-10-10 15:27:39 +0300

    done

--- src/views/contectMe/ContactMe.jsx
+++ src/views/contectMe/ContactMe.jsx
@@ -23,19 +23,19 @@ export const ContactMe = () => {
           <h1>Contact Me</h1>
           <form className="form" ref={form} onSubmit={sendEmail}>
               <div className="form-group has-feedback">
-                  <label for="name"></label>
+                  <label htmlFor="name"></label>
                   <input type="text" className="form-control" id="name" placeholder="Your name" name="user_name" required/>
-                  <i class="bi bi-person-fill" role="img" aria-label="GitHub"></i>
+                  <i className="bi bi-person-fill" role="img" aria-label="GitHub"></i>
               </div>
               <div className="form-group has-feedback">
-                  <label for="email"></label>
+                  <label htmlFor="email"></label>
                   <input type="email" className="form-control" id="email" placeholder="Enter your email" name="user_email" required/>
-                  <i class="bi bi-envelope-fill"></i>
+                  <i className="bi bi-envelope-fill"></i>
               </div>
               <div className="form-group has-feedback">
-                <label for="message"></label>
+                <label htmlFor="message"></label>
                 <textarea className="form-control" rows="8" id="message2" placeholder="Your Message" name="message" required></textarea>
-                <i class="bi bi-pencil-fill"></i>
+                <i className="bi bi-pencil-fill"></i>
               </div>
               <input type="submit" value="Send me" className="btn btn-secondary"/>
         </form>
@@ -43,10 +43,10 @@ export const ContactMe = () => {
     <div className="footer">
       <h1 style={{paddingTop:'15px'}}>Connect with me</h1>
       <ul className="social-links" style={{listStyle:"none"}}>
-        <a target="_blank" href="https://www.linkedin.com/in/yardencohavi/" class="pi bi-linkedin"></a>
-        <a target="_blank" href="https://github.com/yardencohavi" class="pi bi-github"></a>
-        <a target="_blank" href="https://www.instagram.com/yardencohavi/" class="pi bi-instagram"></a>
-        <a target="_blank" href="https://www.facebook.com/yardencohavi/" class="pi bi-facebook"></a>
+        <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/yardencohavi/" className="pi bi-linkedin"></a>
+        <a target="_blank" rel="noreferrer" href="https://github.com/yardencohavi" className="pi bi-github"></a>
+        <a target="_blank" rel="noreferrer" href="https://www.instagram.com/yardencohavi/" className="pi bi-instagram"></a>
+        <a target="_blank" rel="noreferrer" href="https://www.facebook.com/yardencohavi/" className="pi bi-facebook"></a>
       </ul>
     </div>      
   </div>
